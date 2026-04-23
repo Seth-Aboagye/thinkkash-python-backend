f011cbe0c276d2cc7d91aa2f444a905fa6b68403
Seth-Aboagye <aboagye@wustl.edu>
2026-04-22 20:18:29 -0400

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -3,7 +3,7 @@ from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 ALGORITHM = "HS256"
 
 def hash_password(password: str) -> str:
